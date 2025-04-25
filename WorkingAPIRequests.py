73805c106991621458216367e228346147dd022a
Lukas Eliassen <luel@itu.dk>
2025-04-25 11:36:23 +0200

    added python file with requests desc

---
+++ WorkingAPIRequests.py
@@ -0,0 +1,13 @@
+'''
+
+Endpoint GET search/restaurants/{tenant}
+
+curl -X GET "https://uk.api.just-eat.io/search/restaurants/uk?searchTerm=London&latlong=51.5074,-0.1278"
+
+https://uk.api.just-eat.io/search/restaurants/uk?searchTerm=London&latlong=51.5074,-0.1278
+
+this produces list of all restaurants in London around the coordinates
+
+
+
+'''
